997127dc55dd9c9808b299c83c75a62fb367d9f2
mbarquin <moises.barquin@gmail.com>
2017-04-05 01:00:26 +0200

    BlogController: Fin del post Instalación, routing y controllers

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -1,18 +1,27 @@
 <?php
 // src/AppBundle/Controller/BlogController.php
 namespace AppBundle\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
 class BlogController extends Controller
 {
     /**
-     * @Route("/blog")
+     * @Route("/blog/{page}", name="blog_list", requirements={"page": "\d+"})
      */
-    public function listAction()
+    public function listAction($page=1)
     {
         return new Response(
-            '<html><body>Reading Blog index page</body></html>'
+            '<html><body>Showing page number: '.$page.'</body></html>'
+        );
+    }
+    /**
+     * @Route("/blog/{title}", name="blog_read", requirements={"title": "\S+"})
+     */
+    public function readAction($title)
+    {
+        return new Response(
+            '<html><body>Showing post:'.$title.'</body></html>'
         );
     }
 }
